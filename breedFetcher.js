6d2f21b560040d39a8c8aa71714d91e4751f36d5
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-26 22:03:13 -0800

    feat- Made fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,30 @@
 const needle = require("needle");
-const breedName = process.argv[2];
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-needle.get(apiUrl, (error, response) => {
-  if (error) {
-    console.error("Error Occured:", error);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  if (response.body.length === 0) {
-    console.log("Breed not found!");
-    return;
-  }
-
-  const breed = response.body[0];
-  console.log("Description:", breed.description);
+  needle.get(apiUrl, (error, response) => {
+    if (error) {
+      // pass the error to the callback
+      callback(error, null);
+      return;
+    }
+    // check if breed is found
+    const data = response.body;
+    if (data.length === 0) {
+      // no breed found
+      callback(null, "Breed not found!");
+      return;
+    }
     
-});
+    // else - return breed description
+    const breed = data[0];
+    callback(null, breed.description);
+      
+  });
+};
+
+
 
 
 
